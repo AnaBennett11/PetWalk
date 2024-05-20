8fa8aabb62c781aa2906b56f938340d7203a45f6
Ana Bennett <anabennett1111@gmail.com>
2024-05-20 11:31:10 -0500

    Create a server to provide further protection of API key

---
+++ public/main.js
@@ -0,0 +1,46 @@
+let map;
+let infoWindow;
+
+window.initMap = function() {
+  console.log("hello, hi this is a test");
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 15,
+  });
+
+  infoWindow = new google.maps.InfoWindow(); // Initialize infoWindow
+
+  // Add a marker for user's current location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const userPos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        map.setCenter(userPos);
+        const marker = new google.maps.Marker({
+          position: userPos,
+          map: map,
+          title: "Your Location",
+        });
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
